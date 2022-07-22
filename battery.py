829f41fe0bdb15a6abe2dfe9e574e9bb26f03012
Kritsadachon <pet300you@gmail.com>
2022-07-22 15:55:11 +0700

    Edit (line 8) from 30 to 29

--- battery.py
+++ battery.py
@@ -5,7 +5,7 @@ battery = psutil.sensors_battery()
 plugged = battery.power_plugged
 percent = battery.percent
 
-if percent <= 30 and plugged!=True:
+if percent <= 29 and plugged!=True:
  
     # pip install py-notifier
     # pip install win10toast
